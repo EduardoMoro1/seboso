46b9059b8a056d885419a7a29db86a48e629ecf3
Eduado <129158742+EduardoMoro1@users.noreply.github.com>
2024-06-10 08:33:52 -0300

    Checkpoint dia 10/06

---
+++ main.js
@@ -0,0 +1,45 @@
+const abaPrincipal = document.querySelector(".aba-principal");
+const abaPerguntas = document.querySelector(".aba-perguntas");
+const abaAlternativas = document.querySelector(".aba-alternativas");
+const abaResultado = document.querySelector(".aba-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Supondo que você seja um agricultor de subsistência, qual seria sua primeira ação para o benefício mútuo entre sua economia e o meio ambiente?",
+        alternativas: [
+            "Fazer uso de uma composteira para a decomposição de certos resíduos orgânicos",
+            "Fazer uso da reciclagem para evitar o acúmulo de resíduos que possivelmente trarão prejuízo ao meio ambiente",
+
+        ]
+    },
+    {
+        enunciado: "Considerando todos os fatores, você deve optar por uma alternativa menos prejudicial ao meio ambiente, você optaria por:",
+        alternativas: [
+            "Agropecuária",
+            "Agricultura",
+
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    }
+]; 
