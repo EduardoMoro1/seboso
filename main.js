838f28374a708e7c77e0e2d25d07a781ab502dec
Eduado <129158742+EduardoMoro1@users.noreply.github.com>
2024-08-12 10:58:56 -0300

    Checkpoint 12/08
    Obs: organizar os arquivos .js em uma pasta js

--- main.js
+++ main.js
@@ -1,70 +1,18 @@
+import {aleatorio} from './aleatorio.js';
+import {perguntas} from './perguntas.js';
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultados = document.querySelector(".caixa-resultados");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = [
-    {
-        enunciado: "Numa situação hipotética, você está vivendo em uma pequena fazenda, qual será sua primeira ação:",
-        alternativas: [
-        {
-            texto: "Plantar vegetais para vender ou comer posteriormente",
-            afirmacao: "Você pensa no futuro, quer promover um sustento básico antes de  tudo",
-        },
-        {
-            texto: "Separar o espaço para o cultivo de animais",
-            afirmacao: "Você pensa como um empreendedor, optou pelo caminho mais lucrativo",
-        }        
-        ]
-    },
-    {
-        enunciado: "Com seu território em mãos, você optaria por uso de agrotóxicos e outros produtos químicos?",
-        alternativas: [
-        {
-            texto: "sim",
-            afirmacao: "Você não se importa com o meio-ambiente ou o ambiente onde mora, apenas quer lucro sem esforço",
-        },
-        {
-            texto: "não",
-            afirmacao: "Você é um agricultor padrão, opta pela segurança do meio-ambiente",
-        }        
-        ]
-    },
-    {
-        enunciado: "Você optaria por introduzir máquinas na sua fazenda ou fará tudo manualmente?",
-        alternativas: [
-        {
-            texto: "Irei introduzir o uso de algumas máquinas para processar meus produtos eficientemente",
-            afirmacao: "Você é agora um empreendedor, seu lucro irá provavelmente aumentar muito, contudo, o meio-ambiente sofrerá com suas ações",
-        },
-        {
-            texto: "Irei optar pelo uso de minhas mãos somente, para evitar gastos desnecessários",
-            afirmacao: "Você quer apenas viver por viver, não quer lucro, apenas uma vida simples",
-        }        
-        ]
-    },
-    {
-        enunciado: "Você pretende ter uma vida simples ou pretende maximizar o lucro?",
-        alternativas: [
-        {
-            texto: "Vou ter uma vida simples, sem me preocupar com despesas ou maquinário",
-            afirmacao: "Você é um grande protetor do meio-ambiente, está preservando tudo do jeito correto e mantendo o ciclo",
-        },
-        {
-            texto: "Vou priorizar o lucro acima de tudo, independente das circunstâncias",
-            afirmacao: "Você é um grande inimigo do meio-ambiente, você não liga para a vegetação, mares, você apenas age sem pensar nos impactos de suas ações",
-        },
-            
-        ]
 
-    },
-    
-];
 
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
+
 
 function mostraPergunta() {
     if (atual >= perguntas.length) {
@@ -77,11 +25,6 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 
-function mostraResultado() {
-    caixaPerguntas.textContent = "";
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "";
-}
 
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
@@ -93,11 +36,16 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " ";
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+    historiaFinal += afirmacoes + "";
     atual++;
     mostraPergunta();
 }
 
+function mostraResultado() {
+    caixaPerguntas.textContent = "Teu Pai de Calcinha";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
 
 mostraPergunta();
---
+++ perguntas.js
@@ -0,0 +1,57 @@
+export const perguntas = [
+    {
+        enunciado: "Numa situação hipotética, você está vivendo em uma pequena fazenda, qual será sua primeira ação:",
+        alternativas: [
+        {
+            texto: "Plantar vegetais para vender ou comer posteriormente",
+            afirmacao: "Você pensa no futuro, quer promover um sustento básico antes de  tudo",
+        },
+        {
+            texto: "Separar o espaço para o cultivo de animais",
+            afirmacao: "Você pensa como um empreendedor, optou pelo caminho mais lucrativo",
+        }        
+        ]
+    },
+    {
+        enunciado: "Com seu território em mãos, você optaria por uso de agrotóxicos e outros produtos químicos?",
+        alternativas: [
+        {
+            texto: "sim",
+            afirmacao: "Você não se importa com o meio-ambiente ou o ambiente onde mora, apenas quer lucro sem esforço",
+        },
+        {
+            texto: "não",
+            afirmacao: "Você é um agricultor padrão, opta pela segurança do meio-ambiente",
+        }        
+        ]
+    },
+    {
+        enunciado: "Você optaria por introduzir máquinas na sua fazenda ou fará tudo manualmente?",
+        alternativas: [
+        {
+            texto: "Irei introduzir o uso de algumas máquinas para processar meus produtos eficientemente",
+            afirmacao: "Você é agora um empreendedor, seu lucro irá provavelmente aumentar muito, contudo, o meio-ambiente sofrerá com suas ações",
+        },
+        {
+            texto: "Irei optar pelo uso de minhas mãos somente, para evitar gastos desnecessários",
+            afirmacao: "Você quer apenas viver por viver, não quer lucro, apenas uma vida simples",
+        }        
+        ]
+    },
+    {
+        enunciado: "Você pretende ter uma vida simples ou pretende maximizar o lucro?",
+        alternativas: [
+        {
+            texto: "Vou ter uma vida simples, sem me preocupar com despesas ou maquinário",
+            afirmacao: "Você é um grande protetor do meio-ambiente, está preservando tudo do jeito correto e mantendo o ciclo",
+        },
+        {
+            texto: "Vou priorizar o lucro acima de tudo, independente das circunstâncias",
+            afirmacao: "Você é um grande inimigo do meio-ambiente, você não liga para a vegetação, mares, você apenas age sem pensar nos impactos de suas ações",
+        },
+            
+        ]
+
+    },
+    
+];
